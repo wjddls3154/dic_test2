a3fb2ead6a47f9ecb7dbd664cf5c8029065aaaa7
k <k@ks-Mac.local>
2018-10-24 08:55:04 -0700

    Initial Commit

---
+++ dic_test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dic_test2
+//
+//  Created by k on 헤이세이 30. 10. 24..
+//  Copyright © 헤이세이 30년 ksh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
